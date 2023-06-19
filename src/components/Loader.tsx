4036170ace380aa7783317f264aab792de5a00a4
rikarani <46773591+rikarani@users.noreply.github.com>
2023-06-19 19:04:13 +0700

    modif dikit

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -16,11 +16,6 @@ function Loader(): React.JSX.Element {
         if (width >= 100) {
           setOpen(false);
           clearInterval(intervalId);
-          setTimeout(() => {
-            if (bgRef.current) {
-              bgRef.current.classList.add("hidden");
-            }
-          }, 300);
         } else {
           width += Math.floor(Math.random() * 5 + 1);
           updateProgress();
@@ -47,15 +42,15 @@ function Loader(): React.JSX.Element {
   }, [open]);
 
   return (
-    <div id="bg" ref={bgRef} className={`${open ? "bg-slate-950" : "bg-transparent"} fixed bottom-0 left-0 right-0 top-0 grid origin-top-left place-items-center px-4 transition-all duration-700 ease-in-out`}>
+    <div ref={bgRef} className="fixed bottom-0 left-0 right-0 top-0 grid place-items-center bg-slate-950 px-4">
       <div>
         <h1 className="text-justify text-6xl font-semibold text-gray-50">Sit Tight</h1>
         <h2 className="text-4xl text-gray-50">While I'm Prepare Everything For You</h2>
         <p ref={textRef} className="mt-1.5 text-gray-50">
           0%
         </p>
-        <div id="progress" className="h-1 overflow-hidden bg-white">
-          <div ref={barRef} id="bar" className="h-1 w-0 bg-red-400"></div>
+        <div id="progress" className="h-1 overflow-hidden rounded-xl bg-white">
+          <div ref={barRef} id="bar" className="h-1 w-0 bg-red-400 transition-all duration-500 ease-in-out"></div>
         </div>
       </div>
     </div>
