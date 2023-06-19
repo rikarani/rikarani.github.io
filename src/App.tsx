f451b7cf5a707c6dd43f1443bdbfe84879bcf01b
rikarani <46773591+rikarani@users.noreply.github.com>
2023-06-19 19:04:39 +0700

    import hook, nambahin transisi

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
 import React, { useState } from "react";
 import { LoaderContext } from "./context/context";
+import { useTransition, animated } from "@react-spring/web";
 import Loader from "./components/Loader";
-import Locker from "./components/Locker";
-import Home from "./components/Home";
+// import Locker from "./components/Locker";
+// import Home from "./components/Home";
 
 function App(): React.JSX.Element {
   const [open, setOpen] = useState<boolean>(true);
 
+  const transition = useTransition(open, {
+    from: { opacity: 1 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  });
+
   return (
     <>
       <LoaderContext.Provider value={{ open, setOpen }}>
-        <Loader />
-        <Locker />
-        <Home />
+        <div className="flex justify-center py-4">
+          <button onClick={() => setOpen((prev) => !prev)} className="rounded-3xl bg-cyan-400 px-4 py-2 text-lg hover:bg-cyan-600 hover:text-gray-100">
+            {open ? "Un-Mount" : "Mount"}
+          </button>
+        </div>
+        {transition(
+          (style, item) =>
+            item && (
+              <animated.div style={style}>
+                <Loader />
+              </animated.div>
+            )
+        )}
       </LoaderContext.Provider>
     </>
   );
