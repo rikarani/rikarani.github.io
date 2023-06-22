8821512d429fba719585f36a779d866d910ed8ef
rikarani <46773591+rikarani@users.noreply.github.com>
2023-06-22 22:08:33 +0700

    apus effect, pake custom hook

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,60 +1,29 @@
-import React, { useEffect, useRef, useContext } from "react";
+import React, { useEffect, useContext } from "react";
+import { useProgressBar } from "../hooks/useProgressBar";
 import { LoadingContext } from "../context/context";
 
-function Loader(): React.JSX.Element {
+function Loading(): React.JSX.Element {
   const { open, setOpen } = useContext(LoadingContext);
-  const bgRef = useRef<HTMLDivElement>(null);
-  const barRef = useRef<HTMLDivElement>(null);
-  const textRef = useRef<HTMLParagraphElement>(null);
+  const progress = useProgressBar(open);
 
   useEffect(() => {
-    let intervalId: number | undefined = undefined;
-    let width = 0;
-
-    const startLoading = () => {
-      intervalId = setInterval(() => {
-        if (width >= 100) {
-          setOpen(false);
-          clearInterval(intervalId);
-        } else {
-          width += Math.floor(Math.random() * 5 + 1);
-          updateProgress();
-        }
-      }, 350);
-    };
-
-    const updateProgress = () => {
-      if (barRef.current && textRef.current) {
-        barRef.current.style.width = `${width}%`;
-        textRef.current.innerText = `${width >= 100 ? 100 : width}%`;
-      }
-    };
-
-    if (width === 0 && open) {
-      startLoading();
+    if (progress >= 100) {
+      setOpen(false);
     }
-
-    return () => {
-      if (intervalId) {
-        clearInterval(intervalId);
-      }
-    };
-  }, [open]);
+  }, [progress]);
 
   return (
-    <div ref={bgRef} className="fixed bottom-0 left-0 right-0 top-0 grid place-items-center bg-slate-950 px-4 font-tulisan">
+    <div className="fixed bottom-0 left-0 right-0 top-0 grid place-items-center bg-slate-950 px-4 font-tulisan">
       <div>
         <h1 className="mb-1.5 text-justify text-6xl font-semibold text-gray-50">Sit Tight</h1>
         <h2 className="text-4xl text-gray-50">While I'm Prepare Everything For You</h2>
-        <p ref={textRef} className="mt-1.5 text-gray-50">
-          0%
-        </p>
+        <p className="mt-1.5 text-gray-50">{progress >= 100 ? 100 : progress}%</p>
         <div id="progress" className="h-1 overflow-hidden rounded-xl bg-white">
-          <div ref={barRef} id="bar" className="h-1 w-0 bg-red-400 transition-all duration-500 ease-in-out"></div>
+          <div id="bar" style={{ width: `${progress}%` }} className="h-1 w-0 bg-red-400 transition-all duration-500 ease-in-out"></div>
         </div>
       </div>
     </div>
   );
 }
 
-export default Loader;
+export default Loading;
