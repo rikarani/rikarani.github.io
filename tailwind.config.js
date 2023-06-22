3a3b96c36bececf23cc2d3d1f15006fe4b3403e3
rikarani <46773591+rikarani@users.noreply.github.com>
2023-06-22 22:12:17 +0700

    extend font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import { fontFamily } from "tailwindcss/defaultTheme";
+
 export default {
   content: ["./index.html", "./src/**/*.tsx"],
   theme: {
     extend: {
+      fontFamily: {
+        tulisan: ["Nunito Sans", "Raleway", ...fontFamily.sans],
+      },
       backgroundImage: {
         hp: "url('../../img/ls-hp-3.jpg')",
         desktop: "url('../../img/bg.jpg')",
