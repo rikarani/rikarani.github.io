f2bebb5869475ca5127efdaf7f072a35524ca9bc
rikarani <46773591+rikarani@users.noreply.github.com>
2023-06-24 03:35:05 +0700

    impar impor, trus pake partikel ny

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,10 +1,19 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext, useCallback } from "react";
+import type { Engine } from "tsparticles-engine";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
 import { useProgressBar } from "../hooks/useProgressBar";
 import { LoadingContext } from "../context/context";
+import { nightSky } from "../config/nightSky";
 
 function Loading(): React.JSX.Element {
   const { open, setOpen } = useContext(LoadingContext);
   const progress = useProgressBar(open);
+
+  const particleInit = useCallback(async (engine: Engine) => {
+    await loadFull(engine);
+  }, []);
 
   useEffect(() => {
     if (progress >= 100) {
@@ -13,16 +22,19 @@ function Loading(): React.JSX.Element {
   }, [progress]);
 
   return (
-    <div className="fixed bottom-0 left-0 right-0 top-0 grid place-items-center bg-slate-950 px-4 font-tulisan">
-      <div>
-        <h1 className="mb-1.5 text-justify text-6xl font-semibold text-gray-50">Sit Tight</h1>
-        <h2 className="text-4xl text-gray-50">While I'm Prepare Everything For You</h2>
-        <p className="mt-1.5 text-gray-50">{progress >= 100 ? 100 : progress}%</p>
-        <div id="progress" className="h-1 overflow-hidden rounded-xl bg-white">
-          <div id="bar" style={{ width: `${progress}%` }} className="h-1 w-0 bg-red-400 transition-all duration-500 ease-in-out"></div>
+    <>
+      <Particles init={particleInit} options={nightSky} />
+      <div className="fixed bottom-0 left-0 right-0 top-0 grid place-items-center px-4 font-tulisan">
+        <div>
+          <h1 className="text-4xl font-bold text-gray-50">Among the expanse of universe light</h1>
+          <h2 className="text-2xl text-gray-50">I found my soul trapped in endless loneliness</h2>
+          <p className="mt-1.5 text-gray-50">{progress >= 100 ? 100 : progress}%</p>
+          <div id="progress" className="h-1 overflow-hidden rounded-xl bg-white">
+            <div id="bar" style={{ width: `${progress}%` }} className="h-1 w-0 bg-red-400 transition-all duration-500 ease-in-out"></div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
