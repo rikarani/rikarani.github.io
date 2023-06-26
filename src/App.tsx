3503c60e4bd9eb5bd2ffbb7f6d95f79591bacbd9
rikarani <46773591+rikarani@users.noreply.github.com>
2023-06-26 21:17:56 +0700

    penyesuaian

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import React, { useState } from "react";
-import { LoadingContext } from "./context/context";
-// import { useTransition, animated } from "@react-spring/web";
+import { LoadingContext } from "./context/LoadingContext";
+import { useTransition, animated } from "@react-spring/web";
 
-// import Loading from "./components/Loading";
-import Lockscreen from "./components/Lockscreen";
+import Loading from "./components/Loading";
+// import Lockscreen from "./components/Lockscreen";
 // import Home from "./components/Home";
 
 function App(): React.JSX.Element {
-  const [open, setOpen] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
 
-  // const transition = useTransition(open, {
-  //   from: { opacity: 0 },
-  //   enter: { opacity: 1 },
-  //   leave: { opacity: 0, delay: 500 },
-  // });
+  const transition = useTransition(loading, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0, delay: 500 },
+  });
 
   return (
-    <LoadingContext.Provider value={{ open, setOpen }}>
-      {/* {transition(
+    <LoadingContext.Provider value={{ loading, setLoading }}>
+      {transition(
         (style, item) =>
           item && (
             <animated.div style={style}>
@@ -26,11 +26,12 @@ function App(): React.JSX.Element {
           )
       )}
       <div className="flex justify-center py-4">
-        <button onClick={() => setOpen((prev) => !prev)} className="rounded-3xl bg-cyan-400 px-4 py-2 text-lg hover:bg-cyan-600 hover:text-gray-100">
+        <button onClick={() => setLoading((prev) => !prev)} className="rounded-3xl bg-cyan-400 px-4 py-2 text-lg hover:bg-cyan-600 hover:text-gray-100">
           Mount
         </button>
-      </div> */}
-      <Lockscreen />
+      </div>
+      {/* <Loading /> */}
+      {/* <Lockscreen /> */}
     </LoadingContext.Provider>
   );
 }
