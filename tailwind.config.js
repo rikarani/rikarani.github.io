02384bbfb03d06107485422b1cbdb7c26e9d1abe
rikarani <46773591+rikarani@users.noreply.github.com>
2023-06-27 23:53:08 +0700

    ubah content, apus extend brightness

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,15 @@
 import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
-  content: ["./index.html", "./src/**/*.tsx"],
+  content: ["./src/**/*.tsx"],
   theme: {
     extend: {
       fontFamily: {
         tulisan: ["Nunito Sans", "Raleway", ...fontFamily.sans],
       },
       backgroundImage: {
-        hp: "url('../../img/ls-hp-3.jpg')",
-        desktop: "url('../../img/bg.jpg')",
-      },
-      brightness: {
-        40: ".40",
+        hp: "url('../../img/miyako.webp')",
+        desktop: "url('../../img/towa.webp')",
       },
     },
   },
