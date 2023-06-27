2ced31e473e9cda27f26e8bc0c6f298ff8328d6b
rikarani <46773591+rikarani@users.noreply.github.com>
2023-06-28 01:01:43 +0700

    atur height

--- src/components/Lockscreen.tsx
+++ src/components/Lockscreen.tsx
@@ -9,7 +9,7 @@ function Lockscreen(): React.JSX.Element {
   const { jam, menit, detik } = useClock();
 
   return (
-    <div className="relative h-screen w-full">
+    <div className="relative h-full w-full overflow-hidden">
       <div className="relative h-full w-full bg-hp bg-cover bg-top before:absolute before:left-0 before:top-0 before:h-full before:w-full before:bg-black/40 before:content-[''] sm:bg-desktop sm:bg-center"></div>
       <div className="absolute left-0 top-0 mt-4 px-4">
         <p className="text-4xl sm:text-gray-200">
