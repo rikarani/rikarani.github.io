1861135201d0ac9b9132def90357f2f122f77b5f
rikarani <46773591+rikarani@users.noreply.github.com>
2023-06-27 23:56:08 +0700

    custom hook buat jam

---
+++ src/hooks/useClock.ts
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+interface Time {
+  jam: number;
+  menit: number;
+  detik: number;
+}
+
+export function useClock(): Time {
+  const [time, setTime] = useState<Time>({
+    jam: new Date().getHours(),
+    menit: new Date().getMinutes(),
+    detik: new Date().getSeconds(),
+  });
+
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      const jam = new Date().getHours();
+      const menit = new Date().getMinutes();
+      const detik = new Date().getSeconds();
+
+      setTime({ jam, menit, detik });
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalID);
+    };
+  }, []);
+
+  return time;
+}
