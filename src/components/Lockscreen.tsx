b965a569d65d16e7c2a240dae000349ef28cc234
rikarani <46773591+rikarani@users.noreply.github.com>
2023-06-30 00:57:55 +0700

    pake custom hooknya

--- src/components/Lockscreen.tsx
+++ src/components/Lockscreen.tsx
@@ -1,23 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { useClock } from "../hooks/useClock";
+import LazyImage from "./partials/LazyImage";
 
 function addZero(time: number): string {
   return time <= 9 ? `0${time}` : `${time}`;
 }
 
 function Lockscreen(): React.JSX.Element {
+  const [ready, setReady] = useState<boolean>(false);
   const { jam, menit, detik } = useClock();
 
   return (
     <div className="relative h-full w-full overflow-hidden">
-      <div className="relative h-full w-full bg-hp bg-cover bg-top before:absolute before:left-0 before:top-0 before:h-full before:w-full before:bg-black/40 before:content-[''] sm:bg-desktop sm:bg-center"></div>
-      <div className="absolute left-0 top-0 mt-4 px-4">
-        <p className="text-4xl sm:text-gray-200">
-          {addZero(jam)} : {addZero(menit)} : {addZero(detik)}
-        </p>
-        <p className="sm:text-gray-200">Selasa, 27 Juni 2023</p>
-        <p className="mt-4 text-lg font-semibold sm:text-gray-200">consectetur adipisicing elit. Ut, provident! Inventore expedita, architecto alias veritatis sunt est at? Similique, illo?</p>
-      </div>
+      {/* background image */}
+      <LazyImage setReady={setReady} />
+      {/* background image */}
+
+      {ready && (
+        <div className="absolute left-0 top-0 mt-4 px-4">
+          <p className="text-4xl sm:text-gray-200">
+            {addZero(jam)} : {addZero(menit)} : {addZero(detik)}
+          </p>
+          <p className="sm:text-gray-200">Selasa, 27 Juni 2023</p>
+          <p className="mt-4 text-lg font-semibold sm:text-gray-200">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, aliquam, doloremque iusto vitae quos deleniti architecto, expedita fugiat omnis corrupti autem eius culpa eligendi rem qui hic dolore reiciendis sequi accusamus
+            ipsa commodi illo soluta aliquid a? Atque, aperiam vitae!
+          </p>
+        </div>
+      )}
     </div>
   );
 }
