268ccbf2b876abba0f50c116c9e9782a830840c8
rikarani <46773591+rikarani@users.noreply.github.com>
2023-07-01 03:23:11 +0700

    banyak perbaikan

--- src/components/Lockscreen.tsx
+++ src/components/Lockscreen.tsx
@@ -1,14 +1,10 @@
 import React, { useState } from "react";
-import { useClock } from "../hooks/useClock";
-import LazyImage from "./partials/LazyImage";
 
-function addZero(time: number): string {
-  return time <= 9 ? `0${time}` : `${time}`;
-}
+import Konten from "./partials/Lockscreen/Konten";
+import LazyImage from "./partials/Lockscreen/LazyImage";
 
 function Lockscreen(): React.JSX.Element {
-  const [ready, setReady] = useState<boolean>(false);
-  const { jam, menit, detik } = useClock();
+  const [ready, setReady] = useState<boolean>(true);
 
   return (
     <div className="relative h-full w-full overflow-hidden">
@@ -16,18 +12,7 @@ function Lockscreen(): React.JSX.Element {
       <LazyImage setReady={setReady} />
       {/* background image */}
 
-      {ready && (
-        <div className="absolute left-0 top-0 mt-4 px-4">
-          <p className="text-4xl sm:text-gray-200">
-            {addZero(jam)} : {addZero(menit)} : {addZero(detik)}
-          </p>
-          <p className="sm:text-gray-200">Selasa, 27 Juni 2023</p>
-          <p className="mt-4 text-lg font-semibold sm:text-gray-200">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, aliquam, doloremque iusto vitae quos deleniti architecto, expedita fugiat omnis corrupti autem eius culpa eligendi rem qui hic dolore reiciendis sequi accusamus
-            ipsa commodi illo soluta aliquid a? Atque, aperiam vitae!
-          </p>
-        </div>
-      )}
+      {ready && <Konten />}
     </div>
   );
 }
