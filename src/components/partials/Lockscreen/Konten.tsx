aab2079a581892dcbcd3eb4b284c969412c08c5a
rikarani <46773591+rikarani@users.noreply.github.com>
2023-07-02 02:23:30 +0700

    abstraksi & impar impor komponen

--- src/components/partials/Lockscreen/Konten.tsx
+++ src/components/partials/Lockscreen/Konten.tsx
@@ -1,27 +1,30 @@
 import React from "react";
-import { useTime } from "../../../hooks/useTime";
-import { useDate } from "../../../hooks/useDate";
-
-function addZero(time: number): string {
-  return time <= 9 ? `0${time}` : `${time}`;
-}
+import Link from "../Link";
+import Waktu from "./Waktu";
+import { Facebook, Instagram, GitHub } from "../Logo";
 
 function Konten(): React.JSX.Element {
-  const { jam, menit, detik } = useTime();
-  const { hari, tanggal, bulan, tahun } = useDate();
-
   return (
     <div className="absolute left-0 top-0 mt-4 px-4">
-      <p className="text-4xl sm:text-gray-200">
-        {addZero(jam)} : {addZero(menit)} : {addZero(detik)}
-      </p>
-      <p className="sm:text-gray-200">
-        {hari} {tanggal} {bulan} {tahun}
-      </p>
-      <p className="mt-4 text-lg font-semibold sm:text-gray-200">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, aliquam, doloremque iusto vitae quos deleniti architecto, expedita fugiat omnis corrupti autem eius culpa eligendi rem qui hic dolore reiciendis sequi accusamus
-        ipsa commodi illo soluta aliquid a? Atque, aperiam vitae!
-      </p>
+      {/* Jam & Tanggal */}
+      <Waktu />
+
+      <div className="mt-4 text-lg font-semibold sm:text-gray-200">
+        <p>Here a quickstart!</p>
+      </div>
+
+      {/* List Sosmed */}
+      <div className="mt-1.5 max-w-max space-y-2.5">
+        <Link to="https://github.com/rikarani">
+          <GitHub />
+        </Link>
+        <Link to="https://facebook.com/rikaraniii/">
+          <Facebook />
+        </Link>
+        <Link to="https://instagram.com/rikaraniii/">
+          <Instagram />
+        </Link>
+      </div>
     </div>
   );
 }
