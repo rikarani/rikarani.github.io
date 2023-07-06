e1b8fcfab177df721c5d9cd9ddd834c18fb08472
rikarani <46773591+rikarani@users.noreply.github.com>
2023-07-06 23:12:46 +0700

    turunin alpha

--- src/components/partials/Lockscreen/LazyImage.tsx
+++ src/components/partials/Lockscreen/LazyImage.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, SetStateAction } from "react";
+import { useEffect, SetStateAction, Dispatch } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { useImage } from "../../../hooks/useImage";
 
 interface LazyImageProps {
-  setReady: React.Dispatch<SetStateAction<boolean>>;
+  setReady: Dispatch<SetStateAction<boolean>>;
 }
 
 function LazyImage({ setReady }: LazyImageProps): React.JSX.Element {
@@ -16,10 +16,7 @@ function LazyImage({ setReady }: LazyImageProps): React.JSX.Element {
     }
   }, [src]);
 
-  return (
-    <div className="relative h-full w-full bg-hp bg-cover bg-top before:absolute before:left-0 before:top-0 before:h-full before:w-full before:bg-black/40 before:content-[''] sm:bg-desktop sm:bg-center"></div>
-    // <div style={{ backgroundImage: `url(${src})` }} className="relative h-full w-full bg-cover bg-top before:absolute before:left-0 before:top-0 before:h-full before:w-full before:bg-black/40 before:content-[''] sm:bg-center"></div>
-  );
+  return <div className="relative h-full w-full bg-hp bg-cover bg-top before:absolute before:left-0 before:top-0 before:h-full before:w-full before:bg-black/30 before:content-[''] sm:bg-desktop sm:bg-center"></div>;
 }
 
 export default LazyImage;
