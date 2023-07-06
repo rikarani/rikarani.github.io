84ed9ee1472ce5021ff19ef1cbb5341f3080a733
rikarani <46773591+rikarani@users.noreply.github.com>
2023-07-07 01:46:47 +0700

    nambahin style buat breakpoint sm

--- src/components/partials/Lockscreen/Waktu.tsx
+++ src/components/partials/Lockscreen/Waktu.tsx
@@ -1,22 +1,25 @@
-import React from "react";
-
 import { useTime } from "../../../hooks/useTime";
 import { useDate } from "../../../hooks/useDate";
+import { useMediaQuery } from "usehooks-ts";
 
 function addZero(time: number): string {
   return time <= 9 ? `0${time}` : `${time}`;
 }
 
 function Waktu(): React.JSX.Element {
-  const { jam, menit, detik } = useTime();
+  const sm: boolean = useMediaQuery("(min-width: 640px)");
+
+  const { jam, menit } = useTime();
   const { hari, tanggal, bulan, tahun } = useDate();
 
   return (
     <>
-      <p className="text-4xl sm:text-gray-200">
-        {addZero(jam)} : {addZero(menit)} : {addZero(detik)}
-      </p>
-      <p className="sm:text-gray-200">
+      <div className="text-5xl font-medium sm:flex sm:gap-2.5 sm:font-normal sm:text-gray-200">
+        <p className="sm:text-7xl">{addZero(jam)}</p>
+        {sm ? <p className="-mt-2 text-7xl">:</p> : ""}
+        <p className="sm:text-7xl">{addZero(menit)}</p>
+      </div>
+      <p className="text-lg font-medium sm:font-normal sm:text-gray-200">
         {hari} {tanggal} {bulan} {tahun}
       </p>
     </>
