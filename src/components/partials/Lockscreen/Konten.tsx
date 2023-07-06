c2f3ee17a0dcbf64fae880e205f4e30365338faf
rikarani <46773591+rikarani@users.noreply.github.com>
2023-07-07 01:47:54 +0700

    bikin tooltip, nambahin style buat breakpoint sm

--- src/components/partials/Lockscreen/Konten.tsx
+++ src/components/partials/Lockscreen/Konten.tsx
@@ -1,29 +1,44 @@
-import React from "react";
+import "tippy.js/dist/tippy.css";
+import { Facebook, Github, Discord } from "../Logo";
+import { useMediaQuery } from "usehooks-ts";
+
 import Link from "../Link";
 import Waktu from "./Waktu";
-import { Facebook, Instagram, GitHub } from "../Logo";
+import Tippy from "@tippyjs/react";
 
 function Konten(): React.JSX.Element {
   return (
-    <div className="absolute left-0 top-0 mt-4 px-4">
+    <div onClick={() => alert("oke")} className="absolute left-0 top-0 h-full w-full px-4 pt-4">
       {/* Jam & Tanggal */}
-      <Waktu />
-
-      <div className="mt-4 text-lg font-semibold sm:text-gray-200">
-        <p>Here a quickstart!</p>
+      <div className="sm:mt-96 sm:pt-24">
+        <Waktu />
       </div>
 
-      {/* List Sosmed */}
-      <div className="mt-1.5 max-w-max space-y-2.5">
-        <Link to="https://github.com/rikarani">
-          <GitHub />
-        </Link>
-        <Link to="https://facebook.com/rikaraniii/">
-          <Facebook />
-        </Link>
-        <Link to="https://instagram.com/rikaraniii/">
-          <Instagram />
-        </Link>
+      <div className="mt-4 text-lg font-semibold sm:text-gray-200">
+        <p className="text-xl">kuliah hahahihi taunya udah semester 5</p>
+        <div className="mt-2 max-w-max space-y-2.5 sm:flex sm:gap-2.5 sm:space-y-0">
+          <div>
+            <Tippy content="Github" arrow={true} placement={useMediaQuery("(min-width: 640px)") ? "bottom" : "right"}>
+              <Link to="https://github.com/rikarani">
+                <Github className="h-11 w-11 sm:h-10 sm:w-10 sm:fill-gray-200" />
+              </Link>
+            </Tippy>
+          </div>
+          <div>
+            <Tippy content="Facebook" arrow={true} placement={useMediaQuery("(min-width: 640px)") ? "bottom" : "right"}>
+              <Link to="https://facebook.com/rikaraniii">
+                <Facebook className="h-11 w-10 sm:h-10 sm:w-11 sm:fill-gray-200" />
+              </Link>
+            </Tippy>
+          </div>
+          <div>
+            <Tippy content="Discord" arrow={true} placement={useMediaQuery("(min-width: 640px)") ? "bottom" : "right"}>
+              <Link to="https://facebook.com/rikaraniii">
+                <Discord className="h-11 w-11 sm:h-10 sm:w-10 sm:fill-gray-200" />
+              </Link>
+            </Tippy>
+          </div>
+        </div>
       </div>
     </div>
   );
