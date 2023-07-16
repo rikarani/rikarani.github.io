1b44064d7d9195ac8143dc945997c877f8d514a0
rikarani <46773591+rikarani@users.noreply.github.com>
2023-07-17 04:14:33 +0700

    perbaiki interface Quotes, pakek hook useFetch

--- src/components/partials/Lockscreen/Konten.tsx
+++ src/components/partials/Lockscreen/Konten.tsx
@@ -1,46 +1,57 @@
-import "tippy.js/dist/tippy.css";
+import { useContext } from "react";
+import { LockscreenContext } from "../../../context/LockscreenContext";
 import { Facebook, Github, Discord } from "../Logo";
-import { useMediaQuery } from "usehooks-ts";
+import { useFetch } from "../../../hooks/useFetch";
 
 import Link from "../Link";
 import Waktu from "./Waktu";
-import Tippy from "@tippyjs/react";
+
+interface Quotes {
+  _id: string;
+  content: string;
+  author: string;
+  tags: string[];
+  authorSlug: string;
+  length: number;
+  dateAdded: string;
+  dateModified: string;
+}
 
 function Konten(): React.JSX.Element {
-  return (
-    <div onClick={() => alert("oke")} className="absolute left-0 top-0 h-full w-full px-4 pt-4">
-      {/* Jam & Tanggal */}
-      <div className="sm:mt-96 sm:pt-24">
-        <Waktu />
-      </div>
+  const [quotes]: Quotes[] = useFetch("https://api.quotable.io/quotes/random?tags=famous-quotes&maxLength=70");
+  const { setLock } = useContext(LockscreenContext);
 
-      <div className="mt-4 text-lg font-semibold sm:text-gray-200">
-        <p className="text-xl">kuliah hahahihi taunya udah semester 5</p>
-        <div className="mt-2 max-w-max space-y-2.5 sm:flex sm:gap-2.5 sm:space-y-0">
-          <div>
-            <Tippy content="Github" arrow={true} placement={useMediaQuery("(min-width: 640px)") ? "bottom" : "right"}>
-              <Link to="https://github.com/rikarani">
-                <Github className="h-11 w-11 sm:h-10 sm:w-10 sm:fill-gray-200" />
-              </Link>
-            </Tippy>
-          </div>
-          <div>
-            <Tippy content="Facebook" arrow={true} placement={useMediaQuery("(min-width: 640px)") ? "bottom" : "right"}>
-              <Link to="https://facebook.com/rikaraniii">
-                <Facebook className="h-11 w-10 sm:h-10 sm:w-11 sm:fill-gray-200" />
-              </Link>
-            </Tippy>
-          </div>
-          <div>
-            <Tippy content="Discord" arrow={true} placement={useMediaQuery("(min-width: 640px)") ? "bottom" : "right"}>
-              <Link to="https://facebook.com/rikaraniii">
-                <Discord className="h-11 w-11 sm:h-10 sm:w-10 sm:fill-gray-200" />
-              </Link>
-            </Tippy>
+  return (
+    quotes && (
+      <div onClick={() => setLock(true)} className="absolute left-0 top-0 h-full w-full px-4 pt-4">
+        <div className="sm:flex sm:h-full sm:max-w-2xl sm:flex-col sm:justify-end">
+          <div className="sm:-translate-y-7">
+            <Waktu />
+
+            <div className="mt-4 box-border text-lg font-semibold sm:text-gray-200">
+              <p className="text-lg text-slate-100 sm:text-xl sm:font-semibold">{quotes.content}</p>
+              <div className="mt-2 max-w-max space-y-4 rounded-md sm:flex sm:gap-4 sm:space-y-0">
+                <div>
+                  <Link to="https://github.com/rikarani">
+                    <Github className="h-9 w-9 fill-slate-50/60 hover:fill-slate-100" />
+                  </Link>
+                </div>
+                <div>
+                  <Link to="/">
+                    <Facebook className="h-9 w-9 fill-slate-50/60 hover:fill-slate-100" />
+                  </Link>
+                </div>
+                <div>
+                  <Link to="/">
+                    <Discord className="h-9 w-9 fill-slate-50/60 hover:fill-slate-100" />
+                  </Link>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    )
   );
 }
 
