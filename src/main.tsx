595fa7d30cc6ea06acb53fd04d66e304329b317a
rikarani <46773591+rikarani@users.noreply.github.com>
2023-07-17 04:22:22 +0700

    nambahin import css

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import "animate.css/animate.min.css";
+import "./css/index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App />);
