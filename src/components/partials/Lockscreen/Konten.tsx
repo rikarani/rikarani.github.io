d03b0481b3be2aba25c8f1c5a71bfd12a01aaff7
rikarani <46773591+rikarani@users.noreply.github.com>
2023-07-17 08:07:51 +0700

    nambahin efek

--- src/components/partials/Lockscreen/Konten.tsx
+++ src/components/partials/Lockscreen/Konten.tsx
@@ -1,7 +1,8 @@
 import { useContext } from "react";
 import { LockscreenContext } from "../../../context/LockscreenContext";
-import { Facebook, Github, Discord } from "../Logo";
+import { Facebook, Github, Discord, Instagram } from "../Logo";
 import { useFetch } from "../../../hooks/useFetch";
+import { AnimationOnScroll } from "react-animation-on-scroll";
 
 import Link from "../Link";
 import Waktu from "./Waktu";
@@ -23,29 +24,36 @@ function Konten(): React.JSX.Element {
 
   return (
     quotes && (
-      <div onClick={() => setLock(true)} className="absolute left-0 top-0 h-full w-full px-4 pt-4">
+      <div onClick={() => setLock(false)} className="absolute left-0 top-0 h-full  px-4 pt-4">
         <div className="sm:flex sm:h-full sm:max-w-2xl sm:flex-col sm:justify-end">
           <div className="sm:-translate-y-7">
             <Waktu />
 
-            <div className="mt-4 box-border text-lg font-semibold sm:text-gray-200">
-              <p className="text-lg text-slate-100 sm:text-xl sm:font-semibold">{quotes.content}</p>
+            <div className="mt-4">
+              <AnimationOnScroll animateIn="animate__fadeInRight" delay={500} duration={0.7} offset={0}>
+                <p className="text-lg font-semibold text-white sm:text-xl">{quotes.content}</p>
+              </AnimationOnScroll>
               <div className="mt-2 max-w-max space-y-4 rounded-md sm:flex sm:gap-4 sm:space-y-0">
-                <div>
+                <AnimationOnScroll animateIn="animate__fadeInLeft" delay={700} duration={0.5} offset={0}>
                   <Link to="https://github.com/rikarani">
-                    <Github className="h-9 w-9 fill-slate-50/60 hover:fill-slate-100" />
+                    <Github className="h-9 w-9 fill-slate-50/60 sm:hover:fill-slate-100" />
                   </Link>
-                </div>
-                <div>
+                </AnimationOnScroll>
+                <AnimationOnScroll animateIn="animate__fadeInLeft" delay={900} duration={0.5} offset={0}>
                   <Link to="/">
-                    <Facebook className="h-9 w-9 fill-slate-50/60 hover:fill-slate-100" />
+                    <Facebook className="h-9 w-9 fill-slate-50/60 sm:hover:fill-slate-100" />
                   </Link>
-                </div>
-                <div>
+                </AnimationOnScroll>
+                <AnimationOnScroll animateIn="animate__fadeInLeft" delay={1100} duration={0.5} offset={0}>
                   <Link to="/">
-                    <Discord className="h-9 w-9 fill-slate-50/60 hover:fill-slate-100" />
+                    <Instagram className="h-9 w-9 fill-slate-50/60 sm:hover:fill-slate-100" />
                   </Link>
-                </div>
+                </AnimationOnScroll>
+                <AnimationOnScroll animateIn="animate__fadeInLeft" delay={1300} duration={0.5} offset={0}>
+                  <Link to="/">
+                    <Discord className="h-9 w-9 fill-slate-50/60 sm:hover:fill-slate-100" />
+                  </Link>
+                </AnimationOnScroll>
               </div>
             </div>
           </div>
