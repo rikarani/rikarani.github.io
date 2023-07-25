73ebf47393e9e42e3b15679cee178b9369e4e8d6
rikarani <46773591+rikarani@users.noreply.github.com>
2023-07-26 03:07:51 +0700

    placeholder dlu

--- src/views/Main.tsx
+++ src/views/Main.tsx
@@ -2,16 +2,29 @@ import { AnimationOnScroll } from "react-animation-on-scroll";
 import { useContext } from "react";
 import { LockscreenContext } from "../context/LockscreenContext";
 
-import Navbar from "./partials/Home/Navbar";
-
-function Main(): React.JSX.Element {
+function Home(): React.JSX.Element {
   const { setLock } = useContext(LockscreenContext);
 
   return (
-    <>
-      <Navbar />
-    </>
+    <div className="flex h-full items-center justify-center">
+      <div>
+        <div className="flex justify-center text-4xl">
+          {"Hello World".split("").map((val, index) => {
+            return (
+              <AnimationOnScroll key={index} animateIn="animate__fadeInUp" delay={index * 150} duration={0.7} offset={0}>
+                {val}
+              </AnimationOnScroll>
+            );
+          })}
+        </div>
+        <div className="mt-6 flex justify-center">
+          <button type="button" onClick={() => setLock(true)} className="rounded bg-cyan-500 px-4 py-2 text-2xl text-white hover:bg-cyan-700">
+            Mount
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
 
-export default Main;
+export default Home;
