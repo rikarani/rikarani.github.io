5dd4bd5925e7004f6b3b1a981e2c30ca23948a7c
rikarani <46773591+rikarani@users.noreply.github.com>
2023-07-30 03:32:44 +0700

    types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,12 @@
 export type Tab = "Profile" | "Skill" | "Project" | "Website Ini?";
+
+export interface Quote {
+  _id: string;
+  content: string;
+  author: string;
+  tags: string[];
+  authorSlug: string;
+  length: number;
+  dateAdded: string;
+  dateModified: string;
+}
