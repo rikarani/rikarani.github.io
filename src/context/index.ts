379408a5d2e07aaeaaedee8cb90edf74154f35d1
rikarani <46773591+rikarani@users.noreply.github.com>
2023-07-30 03:31:49 +0700

    entry point buat context

---
+++ src/context/index.ts
@@ -0,0 +1,6 @@
+import { LoadingContext } from "./LoadingContext";
+import { LockscreenContext } from "./LockscreenContext";
+import { SidebarContext } from "./SidebarContext";
+import { TabContext } from "./TabContext";
+
+export { LoadingContext, LockscreenContext, SidebarContext, TabContext };
