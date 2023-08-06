7cab3aa618eca6a4d66828d5f2b289d676f72107
rikarani <46773591+rikarani@users.noreply.github.com>
2023-08-06 22:21:35 +0700

    apus extend backgroundImage

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,21 +11,10 @@ export default {
       backgroundImage: {
         hp: "url('../../img/mashiro.webp')",
         desktop: "url('../../img/pagi.webp')",
-        "beams-0": "url('../../img/beams/0.png')",
-        "beams-1": "url('../../img/beams/1.png')",
-        "beams-4": "url('../../img/beams/4.png')",
-        "beams-5": "url('../../img/beams/5.png')",
-        "beams-6": "url('../../img/beams/6.png')",
         "beams-7": "url('../../img/beams/7.png')",
-        "beams-8": "url('../../img/beams/8.png')",
       },
       colors: {
         primary: "#181717", // github
-        fesbuk: "#1877F2",
-        ig: "#E4405F",
-        discord: "#5865F2",
-        wasap: "#25D366",
-        linkedin: "#0A66C2",
       },
     },
   },
