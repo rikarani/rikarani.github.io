e809ec9b6a29f97f4ce62ebf579a8f6dccd25927
rikarani <46773591+rikarani@users.noreply.github.com>
2023-08-17 19:47:21 +0700

    ubah short circuit

--- src/components/partials/Link.tsx
+++ src/components/partials/Link.tsx
@@ -9,7 +9,7 @@ interface LinkProps {
 
 function Link({ to, children, openInNewTab = true, className }: LinkProps): React.JSX.Element {
   return (
-    <a href={to} target={openInNewTab ? "_blank" : "_self"} onClick={(e) => e.stopPropagation()} className={className || ""}>
+    <a href={to} target={openInNewTab ? "_blank" : "_self"} onClick={(e) => e.stopPropagation()} className={className || undefined}>
       {children}
     </a>
   );
