ac14d73944a3c2d8f1fa57771cee61ca515ea931
rikarani <46773591+rikarani@users.noreply.github.com>
2023-08-23 01:26:59 +0700

    apus import path

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,8 +1,7 @@
 import { LoadingContext } from "./LoadingContext";
-import { LockscreenContext } from "./LockscreenContext";
 import { SidebarContext } from "./SidebarContext";
 import { TabContext } from "./TabContext";
 import { DialogContext } from "./DialogContext";
 import { SkillContext } from "./SkillContext";
 
-export { LoadingContext, LockscreenContext, SidebarContext, TabContext, DialogContext, SkillContext };
+export { LoadingContext, SidebarContext, TabContext, DialogContext, SkillContext };
