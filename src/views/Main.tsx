2166b2ab6b220158d71f6edec73b29d496155a30
rikarani <46773591+rikarani@users.noreply.github.com>
2023-08-24 01:16:28 +0700

    ya

--- src/views/Main.tsx
+++ src/views/Main.tsx
@@ -1,27 +1,28 @@
 import { useState } from "react";
-import { TabContext } from "../context";
-import type { Tab } from "../types";
+import { TabContext } from "@/context";
+import type { Tab } from "@/types";
 
-import Navbar from "../components/Navbar";
-import Profile from "../components/Profile";
-import Skills from "../components/Skills";
-import Projects from "../components/Projects";
-import About from "../components/About";
+import Navbar from "@/components/Navbar";
+import Profile from "@/components/Profile";
+import Skills from "@/components/Skills";
+import Projects from "@/components/Projects";
+import About from "@/components/About";
+import Layout from "@/components/Layout";
 
-function Home(): React.JSX.Element {
+function Main(): React.JSX.Element {
   const [activeTab, setActiveTab] = useState<Tab>("Profile");
 
   return (
     <TabContext.Provider value={{ activeTab, setActiveTab }}>
       <Navbar />
-      <main className="flex-1 bg-slate-900 bg-beams-7 bg-center px-4 pt-4 md:px-6">
+      <Layout>
         {activeTab === "Profile" ? <Profile /> : null}
         {activeTab === "Skill" ? <Skills /> : null}
         {activeTab === "Project" ? <Projects /> : null}
         {activeTab === "Website Ini?" ? <About /> : null}
-      </main>
+      </Layout>
     </TabContext.Provider>
   );
 }
 
-export default Home;
+export default Main;
