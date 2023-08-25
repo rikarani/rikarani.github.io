0def3cfeaf58f18177c03e617c78913837577a95
rikarani <46773591+rikarani@users.noreply.github.com>
2023-08-26 01:29:40 +0700

    ganti warna

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,7 @@ function Navbar(): React.JSX.Element {
     <SidebarContext.Provider value={{ isOpen, setIsOpen }}>
       <Sidebar />
 
-      <div className="bg-[#151b3d] px-6 py-4">
+      <div className="bg-primary px-6 py-4">
         <nav className="flex items-center justify-end xl:mx-auto xl:max-w-4xl">
           <div className="md:hidden" onClick={() => setIsOpen(true)}>
             <Hamburger className="h-6 w-6 stroke-slate-200 stroke-2 hover:cursor-pointer" />
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -19,12 +19,12 @@ export default function Sidebar(): React.JSX.Element {
 
   return (
     <>
-      {isOpen && <div className="fixed left-0 top-0 h-full w-full bg-black/50 md:hidden"></div>}
+      {isOpen && <div className="fixed left-0 top-0 h-full w-full bg-primary/50 md:hidden"></div>}
 
       {sidebarTransition((style, item) => {
         return (
           item && (
-            <animated.aside style={style} className="fixed bottom-0 right-0 top-0 z-10 w-3/4 divide-y-2 divide-slate-700 bg-slate-900 px-6 py-4 md:hidden">
+            <animated.aside style={style} className="fixed bottom-0 right-0 top-0 z-10 w-3/4 divide-y divide-border bg-primary px-6 py-4 md:hidden">
               <div className="mb-4 flex items-center justify-end">
                 <div className="hover:cursor-pointer" onClick={() => setIsOpen(false)}>
                   <XMark className="h-6 w-6 stroke-slate-200 stroke-2" />
