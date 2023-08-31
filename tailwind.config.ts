b352401baee265b5cce795019692adf74a7f2dd9
rikarani <46773591+rikarani@users.noreply.github.com>
2023-09-01 01:08:09 +0700

    extend fill

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,15 +13,29 @@ export default {
         secondary: "#fafafa",
         border: "#1c1c1e",
       },
+      fill: {
+        html: "#E34F26",
+        css: "#1572B6",
+        javascript: "#F7DF1E",
+        react: "#61DAFB",
+        tailwind: "#06B6D4",
+        php: "#777BB4",
+        laravel: "#FF2D20",
+      },
       keyframes: {
         floating: {
           "0%": { transform: "translate(0, 0)" },
           "50%": { transform: "translate(0, 15px)" },
           "100%": { transform: "translate(0, -0px)" },
         },
+        rotate: {
+          from: { rotate: "0deg" },
+          to: { rotate: "360deg" },
+        },
       },
       animation: {
-        floating: "floating 3s infinite ease-in-out",
+        floating: "floating 3s ease-in-out infinite",
+        rotate: "rotate 20s linear infinite",
       },
     },
   },
