505358ce418bd5fa0752db6b4d91699865c172bb
rikarani <46773591+rikarani@users.noreply.github.com>
2023-09-01 01:14:31 +0700

    buang apalah

--- src/components/partials/MenuButton.tsx
+++ src/components/partials/MenuButton.tsx
@@ -5,7 +5,7 @@ export default function MenuButton({ type, onClick, text }: MenuButton): React.J
     <button
       type={type}
       onClick={onClick}
-      className="relative block w-full rounded-lg text-start text-xl text-gray-300 after:absolute after:bottom-0 after:left-0 after:h-0.5 after:origin-bottom-right after:scale-x-0 after:bg-slate-300/70 after:transition-transform after:duration-300 after:ease-in-out hover:after:w-full hover:after:origin-bottom-left hover:after:scale-x-100 md:max-w-max"
+      className="relative block w-full rounded-lg text-start text-xl text-gray-300 after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-full after:origin-bottom-right after:scale-x-0 after:bg-slate-300/70 after:transition-transform after:duration-300 after:ease-in-out hover:after:origin-bottom-left hover:after:scale-x-100 md:max-w-max"
     >
       {text}
     </button>
