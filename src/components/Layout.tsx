d38935c19255c470a578553e7d02b3fbfcfbfa60
rikarani <46773591+rikarani@users.noreply.github.com>
2023-08-31 22:44:00 +0700

    ubah padding

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }): React.JSX.Element {
   return (
-    <main className="bg-beams-7 flex-1 bg-primary bg-center px-4 pt-4 md:px-6">
+    <main className="bg-beams-7 flex-1 bg-primary bg-center px-4 pt-2 md:px-6">
       <div className="xl:mx-auto xl:max-w-6xl">{children}</div>
     </main>
   );
