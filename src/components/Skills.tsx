c6caa73435a75f6d4999b8b338f003405e85e040
rikarani <46773591+rikarani@users.noreply.github.com>
2023-09-02 23:10:00 +0700

    refaktor komponen

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,6 +1,6 @@
 import { useState, useMemo } from "react";
 import { SkillList as skills } from "@/config/SkillList";
-import { DialogContext, SkillContext } from "@/context";
+import { DialogContext } from "@/context";
 import { Disclosure } from "@headlessui/react";
 
 import Skill from "./partials/Skill";
@@ -21,37 +21,48 @@ export default function Skills(): React.JSX.Element {
 
       <div className="mt-4">
         <DialogContext.Provider value={{ isOpen, setIsOpen }}>
-          <SkillContext.Provider value={{ skill, setSkill }}>
-            <Disclosure defaultOpen>
-              {({ open }) => {
-                return (
-                  <>
-                    <Disclosure.Button className={`mb-2 flex w-full items-center justify-between text-secondary`}>
-                      <p className="text-lg font-medium">lorem ipsum dolor sit amet</p>
-                      <ArrowUp className={`h-6 w-6 transition-all duration-300 ease-in-out ${open ? "rotate-180" : undefined}`} />
-                    </Disclosure.Button>
-                    <Disclosure.Panel className="md:grid md:grid-cols-3 md:gap-6">
-                      {validSkills.map(({ stackname, description, logo }) => {
-                        return <Skill key={stackname} stackname={stackname} description={description} logo={logo} />;
-                      })}
-                    </Disclosure.Panel>
-                  </>
-                );
-              }}
-            </Disclosure>
+          <Disclosure defaultOpen>
+            {({ open }) => {
+              return (
+                <>
+                  <Disclosure.Button className={`mb-2 flex w-full items-center justify-between text-secondary`}>
+                    <p className="text-lg font-medium">lorem ipsum dolor sit amet</p>
+                    <ArrowUp className={`h-6 w-6 transition-all duration-300 ease-in-out ${open ? "rotate-180" : undefined}`} />
+                  </Disclosure.Button>
+                  <Disclosure.Panel className="md:grid md:grid-cols-3 md:gap-6">
+                    {validSkills.map(({ stackname, description, logo }) => {
+                      function handleClick() {
+                        setIsOpen(true);
+                        setSkill(stackname);
+                      }
 
-            <SumberBelajar skill={skill}>
-              <ol className="list-inside list-['-_'] space-y-1">
-                {links[0]?.sumberBelajar.map(({ nama, link }) => {
-                  return (
-                    <li key={nama}>
-                      <Link to={link}>{nama}</Link>
-                    </li>
-                  );
-                })}
-              </ol>
-            </SumberBelajar>
-          </SkillContext.Provider>
+                      return (
+                        <Skill key={stackname} onClick={handleClick}>
+                          <Skill.Logo>{logo}</Skill.Logo>
+                          <div>
+                            <Skill.Name>{stackname}</Skill.Name>
+                            <Skill.Description>{description}</Skill.Description>
+                          </div>
+                        </Skill>
+                      );
+                    })}
+                  </Disclosure.Panel>
+                </>
+              );
+            }}
+          </Disclosure>
+
+          <SumberBelajar skill={skill}>
+            <ol className="list-inside list-['-_'] space-y-1">
+              {links[0]?.sumberBelajar.map(({ nama, link }) => {
+                return (
+                  <li key={nama}>
+                    <Link to={link}>{nama}</Link>
+                  </li>
+                );
+              })}
+            </ol>
+          </SumberBelajar>
         </DialogContext.Provider>
       </div>
     </div>
