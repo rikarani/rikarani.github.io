c41713bdd786527b8daad194ba2b6562fe18359d
rikarani <46773591+rikarani@users.noreply.github.com>
2023-09-03 20:45:05 +0700

    balikin StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "animate.css/animate.min.css";
 import "./css/index.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App />);
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
