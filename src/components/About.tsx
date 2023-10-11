396a7dc759f88712db83186968758533d4a1a330
rikarani <46773591+rikarani@users.noreply.github.com>
2023-10-11 15:22:35 +0700

    konten placeholder

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,8 +1,8 @@
 export default function About(): React.JSX.Element {
   return (
     <div className="text-gray-300">
-      <h1>About</h1>
-      <h6>dan yang lainnya</h6>
+      <h1 className="text-3xl font-medium">About</h1>
+      <h6 className="bg-gradient-to-r from-sky-500 to-sky-300 bg-clip-text text-2xl font-semibold text-transparent">Dan yang lainnya</h6>
     </div>
   );
 }
