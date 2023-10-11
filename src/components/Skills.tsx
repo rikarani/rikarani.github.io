9b69fcb25a57ec565f3087d3d38a3581c812008d
rikarani <46773591+rikarani@users.noreply.github.com>
2023-10-11 15:22:07 +0700

    ganti style

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -14,7 +14,7 @@ export default function Skills(): React.JSX.Element {
   return (
     <DialogContext.Provider value={{ isDialogOpen, setIsDialogOpen }}>
       <div className="text-gray-300">
-        <h1 className="text-3xl font-medium">Skills</h1>
+        <h1 className="text-3xl font-medium lg:text-4xl">Skills</h1>
         <h6 className="bg-gradient-to-r from-sky-500 to-sky-300 bg-clip-text text-2xl font-semibold text-transparent">Dan Sumber Belajarnya</h6>
       </div>
 
